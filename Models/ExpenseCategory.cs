e23176b877746a1bdea4f3e0fc3a94302c840862
Matias <megt_user@hotmail.com>
2020-04-03 22:37:06 +0200

    Add models

---
+++ Models/ExpenseCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeBudgetWf.Models
+{
+    public class ExpenseCategory
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+
+        public IList<Transaction> Transactions { get; set; }
+    }
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeBudgetWf.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public DateTime DateOfTransaction { get; set; }
+        public DateTime DateOfregistration { get; set; }
+        public string Description { get; set; }
+        public double Amount { get; set; }
+        public double Balance { get; set; }
+        public string OthersDetails { get; set; }
+
+
+        public int ExpenseCategoryId { get; set; }
+        public ExpenseCategory ExpenseCategory { get; set; }
+
+
+    }
+}
