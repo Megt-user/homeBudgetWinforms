a9796f9ab6b63139e1d86537e91a98c99609b012
Matias <megt_user@hotmail.com>
2020-04-17 13:13:03 +0200

    Add Data context

---
+++ DataContext.cs
@@ -0,0 +1,15 @@
+﻿using HomeBudgetWf.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HomeBudgetWf
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option)
+        {
+        }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<ExpenseCategory> ExpenseCategories { get; set; }
+    }
+}
