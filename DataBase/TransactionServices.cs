430e0521aad6b7b6c3bc9fba3b26c7c59a28bca9
Matias Gonzalez Tognoni <megt_user@hotmail.com>
2020-04-20 12:07:21 +0200

    Add serilog to class - test

--- DataBase/TransactionServices.cs
+++ DataBase/TransactionServices.cs
@@ -5,6 +5,8 @@ using System.Text;
 using HomeBudgetWf.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace HomeBudgetWf.DataBase
 {
@@ -12,9 +14,13 @@ namespace HomeBudgetWf.DataBase
     {
 
         private DataContext _dataContext;
-        public TransactionServices()
+        private readonly IConfiguration _configuration;
+
+        public TransactionServices(IConfiguration configuration)
         {
-            _dataContext = new DesignTimeDbContextFactory().CreateDbContext(new String[] { });
+            _configuration = configuration;
+            Log.Information("the DB ConnectionString Is:{ConnectionString}", _configuration.GetConnectionString("default"));
+            _dataContext = new DesignTimeDbContextFactory(configuration).CreateDbContext(new String[] { });
         }
 
         public void AddNewData(Transaction transaction)
@@ -40,6 +46,17 @@ namespace HomeBudgetWf.DataBase
 
         public  void AddTestdata()
         {
+            var structuredData = new StructuredData();
+            var simpleData = "This is a string.";
+
+            // Use the static Serilog.Log class for logging.
+            Log.Verbose("Here's a Verbose message.");
+            Log.Debug("Here's a Debug message. Only Public Properties (not fields) are shown on structured data. Structured data: {@sampleData}. Simple data: {simpleData}.", structuredData, simpleData);
+            Log.Information(new Exception("Exceptions can be put on all log levels"), "Here's an Info message.");
+            Log.Warning("Here's a Warning message.");
+            Log.Error(new Exception("This is an exception."), "Here's an Error message.");
+            Log.Fatal("Here's a Fatal message.");
+
             _dataContext.Transactions.Add(new Transaction()
             {
                 DateOfTransaction = new DateTime(2020, 04, 17),
