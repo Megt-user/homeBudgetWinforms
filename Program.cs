fbe6570553d79315035d8b6066de64f44dc5f4e3
Matias Gonzalez Tognoni <megt_user@hotmail.com>
2020-04-20 12:11:17 +0200

    Start Serilog
    Add Comment

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Extensions.Configuration;
+using Serilog;
 using HomeBudgetWf.DataBase;
+
+// https://stackoverflow.com/a/27509005
 
 namespace HomeBudgetWf
 {
@@ -14,18 +15,47 @@ namespace HomeBudgetWf
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             SetUpConfiguration();
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TransactionForm());
+            SetupStaticLogger();
+            try
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Log.Information("start App");
+                RunApp();
+                Application.Run(new TransactionForm());
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
         private static void SetUpConfiguration()
         {
             _Iconfiguration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
                 .Build();
         }
+        private static void SetupStaticLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(_Iconfiguration)
+                //.WriteTo.Console()
+                .CreateLogger();
+        }
+        private static void RunApp()
+        {
+            // Do not pass any logger in via Dependency Injection, as the class will simply reference the static logger.
+            var classThatLogs = new TransactionServices(_Iconfiguration);
+
+            classThatLogs.AddTestdata();
+        }
+
     }
 }
